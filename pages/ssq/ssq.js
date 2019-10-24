cd3f2c694cfaa9979aa4808a649e11039016c193
tangweikun <819573105@qq.com>
2019-10-24 13:53:01 +0800

    :sparkles: refresh while reach bottom

--- pages/ssq/ssq.js
+++ pages/ssq/ssq.js
@@ -3,21 +3,32 @@ const app = getApp();
 Page({
   data: {
     lotteryList: [],
-    foo: '1,2,3',
+    page: 1,
   },
 
   onLoad: function() {
+    this._loadMore();
+  },
+
+  onReachBottom: function() {
+    this._loadMore();
+  },
+
+  _loadMore: function() {
     const that = this;
+    wx.showToast({ title: '加载中', icon: 'loading', duration: 1000 });
     wx.request({
-      url:
-        'https://apis.juhe.cn/lottery/history?key=96ba11ab9a0c841e566060163bcd1128&lottery_id=ssq&page_size=5&page=1',
+      url: `https://apis.juhe.cn/lottery/history?key=96ba11ab9a0c841e566060163bcd1128&lottery_id=ssq&page_size=8&page=${that.data.page}`,
       success: function(res) {
-        console.log(res.data.result.lotteryResList);
         that.setData({
-          lotteryList: res.data.result.lotteryResList.map(item => ({
-            ...item,
-            lottery_res: item.lottery_res.split(','),
-          })),
+          lotteryList: [
+            ...that.data.lotteryList,
+            ...res.data.result.lotteryResList.map(item => ({
+              ...item,
+              lottery_res: item.lottery_res.split(','),
+            })),
+          ],
+          page: that.data.page + 1,
         });
       },
     });
